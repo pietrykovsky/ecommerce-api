fb1cee16f44909e921a63b4956866c6d25757390
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-11-02 20:30:19 +0100

    fixed cart session variable, refactor

--- api/cart/cart.py
+++ api/cart/cart.py
@@ -14,12 +14,29 @@ class Cart:
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session[settings.CART_SETTINGS_ID] = {}
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def __iter__(self):
+        """Iterate over all products in the cart and fetch them from the database."""
+        product_ids = self.cart.keys()
+        products = models.Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """Return the number of products in the cart."""
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
         """Add a product to the cart or change its quantity."""
-        product_id = str(product_id)
+        product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {
                 'quantity': 0,
@@ -50,20 +67,3 @@ class Cart:
         """Delete the cart from the session."""
         del self.session[settings.CART_SESSION_ID]
         self.save()
-
-    def __iter__(self):
-        """Iterate over all products in the cart and fetch them from the database."""
-        product_ids = self.cart.keys()
-        products = models.Product.objects.filter(id__in=product_ids)
-
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product'] = product
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
-
-    def __len__(self):
-        """Return the number of products in the cart."""
-        return sum(item['quantity'] for item in self.cart.values())
