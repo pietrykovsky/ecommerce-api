80b8d79987acc4bfbf8f566d3a7cda1943f2355c
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-11-02 11:51:55 +0100

    created cart class

--- api/cart/cart.py
+++ api/cart/cart.py
@@ -19,4 +19,51 @@ class Cart:
 
     def add(self, product, quantity=1, update_quantity=False):
         """Add a product to the cart or change its quantity."""
-        pass
+        product_id = str(product_id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': 0,
+                'price': str(product.price),
+            }
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        """Mark the cart as modified."""
+        self.session.modified = True
+
+    def remove(self, product):
+        """Remove a product from the cart."""
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def get_total_price(self):
+        """Return the total price of items in the cart."""
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        """Delete the cart from the session."""
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    def __iter__(self):
+        """Iterate over all products in the cart and fetch them from the database."""
+        product_ids = self.cart.keys()
+        products = models.Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """Return the number of products in the cart."""
+        return sum(item['quantity'] for item in self.cart.values())
